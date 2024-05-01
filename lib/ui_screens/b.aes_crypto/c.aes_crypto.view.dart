c6606f3a9fc7281c7565c944bee8dc7b5a03147b
annisa <annisaputriwahyuni46@gmail.com>
2024-05-02 01:57:42 +0700

    style: add credit

--- lib/ui_screens/b.aes_crypto/c.aes_crypto.view.dart
+++ lib/ui_screens/b.aes_crypto/c.aes_crypto.view.dart
@@ -26,71 +26,68 @@ class AesCryptoView extends StatelessWidget {
                 color: Colors.grey.withOpacity(0.1),
                 child: Center(
                   child: Container(
-                      constraints: const BoxConstraints(maxWidth: 400),
-                      child: OnFormBuilder(
-                        listenTo: _dt.rxForm,
-                        builder: () {
-                          return Padding(
-                            padding: const EdgeInsets.all(10),
-                            child: SingleChildScrollView(
-                              child: Column(
-                                children: [
-                                  const SizedBoxH(50),
-                                  const AesCryptoText(),
-                                  const SizedBoxH(15),
-                                  OnReactive(
-                                    () => Row(
-                                      children: [
-                                        Checkbox(
-                                          value: _dt.rxIsCustomKey.st,
-                                          onChanged: (value) {
-                                            _dt.rxIsCustomKey.setState((s) => value);
-                                          },
-                                        ),
-                                        const Text('Encrypt with a custom secret key (length = 32)'),
-                                      ],
-                                    ),
+                    constraints: const BoxConstraints(maxWidth: 400),
+                    child: OnFormBuilder(
+                      listenTo: _dt.rxForm,
+                      builder: () {
+                        return Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: SingleChildScrollView(
+                            child: Column(
+                              children: [
+                                const SizedBoxH(50),
+                                const AesCryptoText(),
+                                const SizedBoxH(15),
+                                OnReactive(
+                                  () => Row(
+                                    children: [
+                                      Checkbox(
+                                        value: _dt.rxIsCustomKey.st,
+                                        onChanged: (value) {
+                                          _dt.rxIsCustomKey.setState((s) => value);
+                                        },
+                                      ),
+                                      const Text('Encrypt with a custom secret key (length = 32)'),
+                                    ],
                                   ),
-                                  Visibility(
-                                    visible: _dt.rxIsCustomKey.st,
-                                    maintainAnimation: true,
-                                    maintainState: true,
-                                    child: const AesCryptoSecretKey(),
-                                  ),
-                                  const SizedBoxH(15),
-                                  // OutlinedButton(
-                                  //   onPressed: () {
-                                  //     _dt.isEncrypt.setState((s) => true);
-                                  //     _ct.encryptDecrypt();
-                                  //   },
-                                  //   child: const Text('Encrypt'),
-                                  // ),
-                                  const AesCryptoButton(
-                                    title: 'Encrypt',
-                                    isEncrypt: true,
-                                  ),
-                                  const SizedBoxH(15),
-                                  const AesCryptoCipherText(),
-                                  const SizedBoxH(15),
-                                  const AesCryptoButton(
-                                    title: 'Decrypt',
-                                    isEncrypt: false,
-                                  ),
-                                  const SizedBoxH(15),
-                                  const AesCryptoPlainText(),
-                                  const SizedBoxH(15),
-                                  OutlinedButton(
-                                    onPressed: () {
-                                      _dt.rxForm.reset();
-                                    },
-                                    child: const Text('Reset'),
-                                  )
-                                ],
-                              ),
+                                ),
+                                Visibility(
+                                  visible: _dt.rxIsCustomKey.st,
+                                  maintainAnimation: true,
+                                  maintainState: true,
+                                  child: const AesCryptoSecretKey(),
+                                ),
+                                const SizedBoxH(15),
+                                const AesCryptoButton(
+                                  title: 'Encrypt',
+                                  isEncrypt: true,
+                                ),
+                                const SizedBoxH(15),
+                                const AesCryptoCipherText(),
+                                const SizedBoxH(15),
+                                const AesCryptoButton(
+                                  title: 'Decrypt',
+                                  isEncrypt: false,
+                                ),
+                                const SizedBoxH(15),
+                                const AesCryptoPlainText(),
+                                const SizedBoxH(15),
+                                OutlinedButton(
+                                  onPressed: () {
+                                    _dt.rxForm.reset();
+                                  },
+                                  child: const Text('Reset'),
+                                ),
+                                const SizedBoxH(20),
+                                const Text('Developed by:'),
+                                const Text('Annisa Putri Wahyuni (227006042)'),
+                              ],
                             ),
-                          );
-                        },
-                      )),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ),
               ),
             ),
